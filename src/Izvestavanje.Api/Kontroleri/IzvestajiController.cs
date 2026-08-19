7cae143a636c667441fd671b9ed0ef727e00d7ad
Jovana <jovanadumic2014@gmail.com>
2026-08-19 13:48:18 +0200

    Dodaj izvestaj termina po kategoriji

---
+++ src/Izvestavanje.Api/Kontroleri/IzvestajiController.cs
@@ -0,0 +1,53 @@
+using Izvestavanje.Api.Dto;
+using Izvestavanje.Api.Podaci;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Izvestavanje.Api.Kontroleri;
+
+[ApiController]
+[Route("api/izvestaji")]
+public class IzvestajiController : ControllerBase
+{
+    private readonly IzvestavanjeKontekst _kontekst;
+
+    public IzvestajiController(
+        IzvestavanjeKontekst kontekst)
+    {
+        _kontekst = kontekst;
+    }
+
+    [HttpGet("termini-po-kategoriji")]
+    public async Task<
+        ActionResult<List<TerminiPoKategorijiDto>>>
+        TerminiPoKategoriji(
+            CancellationToken cancellationToken)
+    {
+        var rezultat =
+            await _kontekst.StavkeRezervacija
+                .AsNoTracking()
+                .GroupBy(stavka => new
+                {
+                    stavka.KategorijaUslugeId,
+                    stavka.NazivKategorije
+                })
+                .Select(grupa =>
+                    new TerminiPoKategorijiDto
+                    {
+                        KategorijaUslugeId =
+                            grupa.Key.KategorijaUslugeId,
+
+                        NazivKategorije =
+                            grupa.Key.NazivKategorije,
+
+                        BrojRezervisanihTermina =
+                            grupa.Count(stavka =>
+                                stavka.IzvestajnaRezervacija
+                                    .Status != "OTKAZANA")
+                    })
+                .OrderBy(x => x.NazivKategorije)
+                .ToListAsync(cancellationToken);
+
+        return Ok(rezultat);
+    }
+}
